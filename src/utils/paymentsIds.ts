48f60a3b37816b88b7948073d9a815f02b1182e9
Haisem Jemal <haisemjemal@Haisems-MacBook-Air.local>
2022-10-13 11:23:19 -0400

    added new payment ids & changed icons location

--- src/utils/paymentsIds.ts
+++ src/utils/paymentsIds.ts
@@ -1,5 +1,12 @@
-export const STANDARD_MONTHLY = "price_1LjifNDMwU9HrNX42gLrnHOS";
-export const STANDARD_YEARLY = "price_1LjimDDMwU9HrNX4rT24UeHt";
+export const STANDARD_MONTHLY =
+  process.env.REACT_APP_PRODUCTION_ENV === "DEV"
+    ? "price_1LjifNDMwU9HrNX42gLrnHOS"
+    : "price_1LsT7dDMwU9HrNX4eue27lRM";
+
+export const STANDARD_YEARLY =
+  process.env.REACT_APP_PRODUCTION_ENV === "DEV"
+    ? "price_1LjimDDMwU9HrNX4rT24UeHt"
+    : "price_1LsTBDDMwU9HrNX47hzd0PVd";
 
 export const UNLIMITED_MONTHLY = "price_1LjimDDMwU9HrNX4rT24UeHt";
 export const UNLIMITED_YEARLY = "price_1Ljin9DMwU9HrNX4V6fN2Qtt";
