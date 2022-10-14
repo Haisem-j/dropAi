87ae85cc06daa32521429d7a9272b6bc37f55932
Haisem Jemal <haisemjemal@Haisems-MacBook-Air.local>
2022-10-14 15:03:35 -0400

    updated payment id

--- src/utils/paymentsIds.ts
+++ src/utils/paymentsIds.ts
@@ -6,7 +6,7 @@ export const STANDARD_MONTHLY =
 export const STANDARD_YEARLY =
   process.env.REACT_APP_PRODUCTION_ENV === "DEV"
     ? "price_1LjimDDMwU9HrNX4rT24UeHt"
-    : "price_1LsTBDDMwU9HrNX47hzd0PVd";
+    : "price_1Lsrk4DMwU9HrNX4Pmhet64d";
 
 export const UNLIMITED_MONTHLY = "price_1LjimDDMwU9HrNX4rT24UeHt";
 export const UNLIMITED_YEARLY = "price_1Ljin9DMwU9HrNX4V6fN2Qtt";
